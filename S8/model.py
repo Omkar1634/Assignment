e57e201be831821cb1d868d02d2202db795a0f76
omkar kharkar <ok22160@essex.ac.uk>
2024-03-24 12:35:17 +0000

    done

--- S8/model.py
+++ S8/model.py
@@ -198,3 +198,112 @@ class S8_Model_BN(nn.Module):
 
 
 
+class S8_Model_LN(nn.Module):
+    def __init__(self):
+        super(S8_Model_LN, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=7, padding=3, bias=False),
+            nn.LayerNorm([8,32,32 ]), 
+            nn.ReLU(),
+            nn.Dropout(0.10),
+
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(8, 4, kernel_size=3, padding=1),
+            nn.LayerNorm([4, 32,32]), 
+            nn.ReLU(),
+             nn.Dropout(0.10),
+
+        )
+
+        self.conv1x1_3 = nn.Sequential(
+                        nn.Conv2d(4, 8, kernel_size=1),
+
+        )
+
+        self.pool_1 = nn.MaxPool2d(2, 2)
+        
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(8, 4, kernel_size=3, padding=1),
+            nn.LayerNorm([4,16,16]),  # Adjust for post-pooling size
+            nn.ReLU(),
+                        nn.Dropout(0.10),
+
+        )
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(4, 10, kernel_size=3, padding=1),
+            nn.LayerNorm([10,16,16]),
+            nn.ReLU(),
+                        nn.Dropout(0.10),
+
+        )
+
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(10, 8, kernel_size=3, padding=1),
+            nn.LayerNorm([8, 16,16]),
+            nn.ReLU(),
+                        nn.Dropout(0.10),
+
+        )
+
+        self.conv1x1_7 = nn.Sequential(
+            nn.Conv2d(8, 8, kernel_size=1),
+            
+        )
+
+        self.pool_2 = nn.MaxPool2d(2, 2)
+        
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(8, 10, kernel_size=3, padding=1),
+            nn.LayerNorm([10,8,8]),
+                        nn.Dropout(0.10),
+                        nn.ReLU(),
+
+        )
+
+        self.conv9 = nn.Sequential(
+            nn.Conv2d(10, 16, kernel_size=3, padding=1),
+            nn.LayerNorm([16,8,8]),
+                        nn.Dropout(0.10),
+                        nn.ReLU(),
+
+        )
+
+        self.conv10 = nn.Sequential(
+            nn.Conv2d(16, 4, kernel_size=1, padding=1),
+                        nn.Dropout(0.10),
+                        nn.LayerNorm([4,10,10]),
+                        nn.ReLU(),
+
+            
+        )
+
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.conv1x1_11 = nn.Sequential(
+            nn.Conv2d(4, 10, kernel_size=1),
+            
+        )
+
+    
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv1x1_3(x)
+        x = self.pool_1(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv1x1_7(x)
+        x = self.pool_2(x)
+        x = self.conv8(x)
+        x = self.conv9(x)
+        x = self.conv10(x)
+        x = self.gap(x)
+        x = self.conv1x1_11(x)
+        x = x.view(x.size(0), -1)  
+        x = F.log_softmax(x, dim=1)
+        return x
